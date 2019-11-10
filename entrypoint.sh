a6897c99e06583a7b8abe6228653223848510a20
sacaciabesac <57591673+sacaciabesac@users.noreply.github.com>
2019-11-10 20:59:12 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 
 # Do deployment
-sh -c "rsync ${INPUT_RSYNC_OPTIONS} --exclude-from="$(git -C $GITHUB_WORKSPACE/ ls-files --exclude-standard -oi --directory > /tmp/excludes; echo /tmp/excludes) -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/ ${INPUT_RSYNC_TARGET}"
+sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/ ${INPUT_RSYNC_TARGET}"
